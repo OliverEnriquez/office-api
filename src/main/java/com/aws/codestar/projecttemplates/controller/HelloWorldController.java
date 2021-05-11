8de9efdcd450fc133c91d6c82834ee5dd3e5115a
Oliver Enriquez <oenriquez@1aauto.com>
2021-05-10 21:02:41 -0600

    Hello world

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -1,15 +1,27 @@
 package com.aws.codestar.projecttemplates.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-
-@RestController
+/**
+ * Basic Spring MVC controller that handles all GET requests.
+ */
+@Controller
+@RequestMapping("/")
 public class HelloWorldController {
 
-   @GetMapping(path = "/")
-   public String helloWorld() {
-      return "Hello Punks";
-   }
+    private final String siteName;
+
+    public HelloWorldController(final String siteName) {
+        this.siteName = siteName;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public String helloWorld() {
+       String hello = "Hello world";
+        return hello;
+    }
 
 }
