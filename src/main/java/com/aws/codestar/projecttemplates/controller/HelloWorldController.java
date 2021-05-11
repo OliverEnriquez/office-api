366a99a865b72b0a06cd37a7d9d6570cadf9077b
Oliver Enriquez <oenriquez@1aauto.com>
2021-05-10 20:40:33 -0600

    Delete files

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -1,15 +1,9 @@
 package com.aws.codestar.projecttemplates.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Basic Spring MVC controller that handles all GET requests.
- */
+
 @RestController
 public class HelloWorldController {
 
